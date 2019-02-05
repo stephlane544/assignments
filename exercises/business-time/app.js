14ae9f95754631e5cb80c83f858ba4c44865103f
Stephanie Lane <stephlane544@gmail.com>
2019-02-05 09:30:52 -0700

    Feb 5, 2019

---
+++ exercises/business-time/app.js
@@ -0,0 +1,9 @@
+#title {
+    background-color: #495c83;
+    font-size: 120px;
+    text-align: center;
+    color: #71331f;
+}
+#grandCanyon {
+    width: 100%;
+}
--- exercises/escape-room/app.js
+++ exercises/escape-room/app.js
@@ -10,21 +10,22 @@ function escapeRoom (selection){
     while(locked){
         selections.push(selection);
         if (selection == 1){
-            return "You have died.";
             locked = false;
+            return "You dead!";
         }else if(selection == 2){
-            console.log("You have found a key.");
+            console.log("You found a key.");
             selection = ask.question("\nWhich option do you choose (1, 2, or 3)?  ");
         }else if(selection == 3){
-            for(var i = 0; i < selections.length; i++){
-                if(selections.includes("2")){
-                    locked = false;
-                    return "You have opened the door with the key you found before. CONGRATULATIONS, you have escaped my escape room!";
-                }else{
-                    console.log("The door is locked. ");
-                    selection = ask.question("\nWhich option do you choose (1, 2, or 3)?  ");
-                }
+            if(selections.includes("2")){
+                locked = false;
+                return "You have opened the door with the key you found before. CONGRATULATIONS, you have escaped my escape room!";
+            }else{
+                console.log("The door is locked. ");
+                selection = ask.question("\nWhich option do you choose (1, 2, or 3)?  ");
             }
+        }else{
+            console.log("That was not an option.");
+            selection = ask.question("\nWhich option do you choose (1, 2, or 3)?  ");
         }
     }
 }
@@ -38,7 +39,17 @@ function escapeRoom (selection){
 
 
 
+var living = true
 
+while(living){
+    if(op 1){
+        living = false
+
+    }else if(op 2){
+        living = false
+    }
+    op3{}
+}
 
 
 
