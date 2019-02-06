f75ecda5dde9a7dcc149dc3ac6dabc01ddb5d3af
Stephanie Lane <stephlane544@gmail.com>
2019-02-06 13:07:08 -0700

    Feb 6, 2019

--- exercises/business-time/app.js
+++ exercises/business-time/app.js
@@ -1,9 +1,11 @@
-#title {
-    background-color: #495c83;
-    font-size: 120px;
-    text-align: center;
-    color: #71331f;
-}
-#grandCanyon {
-    width: 100%;
+document.getElementById("dropbtn").addEventListener("click", dropdown);
+var clickCounter = 0
+function dropdown(){
+    clickCounter++;
+    if (clickCounter % 2 == 1){
+        document.getElementsByClassName("dropdown-content")[0].classList = "show";
+    }else{ 
+        document.getElementsByClassName("show")[0].classList = "dropdown-content";
+    }
+
 }
